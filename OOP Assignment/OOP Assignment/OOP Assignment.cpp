ee8b3883d18c1d65a91ad9323d7446ceba5e5867
kirano99 <34285686+kirano99@users.noreply.github.com>
2018-12-06 11:55:36 +0000

    Change 1

--- OOP Assignment/OOP Assignment/OOP Assignment.cpp
+++ OOP Assignment/OOP Assignment/OOP Assignment.cpp
@@ -5,12 +5,12 @@
 using namespace std;
 
 
-double CalcSSD(double matrix1[3][3], double matrix2[3][3], int size) {
+double CalcSSD(vector<vector<double>*> matrix1[3][3], vector<vector<double>*> matrix2[3][3]) {
 	double SumSquareDiff = 0;
 	double difference = 0;
 	for (int i = 0; i <= 3 - 1; i++) {
 		for (int j = 0; j <= 3 - 1; j++) {
-			difference = matrix1[i][j] - matrix2[i][j];
+			difference = (*matrix1[i])[j] - (*matrix2[i])[j];
 			SumSquareDiff += difference * difference;
 		}
 	}
@@ -24,15 +24,11 @@ int main(){
 	const int wallyrows = 3;
 	const int wallycols = 3;
 
-
-
-	double baseimagear[baserows][basecols] = {
-
-		{1,6,4,7,2,6,3,8,1,3,5},
-		{5,8,2,4,8,7,2,9,1,3,6},
-		{1,5,8,3,8,2,4,6,7,9,8},
-		{6,1,8,3,8,2,6,4,8,9,9}
-
+	double baseimagearray[baserows][basecols] = {
+		{ 1,6,4,7,2,6,3,8,1,3,5 },
+		{ 5,8,2,4,8,7,2,9,1,3,6 },
+		{ 1,5,8,3,8,2,4,6,7,9,8 },
+		{ 6,1,8,3,8,2,6,4,8,9,9 }
 	};
 
 	double wallyar[wallyrows][wallycols] = {
@@ -43,21 +39,30 @@ int main(){
 
 	};
 
-	
+	vector<vector<double>> baseimagear[baserows][basecols];
+
+
+
+
+
+
+
+
 	for (int i = 0; i <= baserows - wallyrows; i++) {
 		for (int j = 0; j <= basecols - wallycols; j++) {
 
-			double tempmatrix[3][3] = {
-				{baseimagear[i][j], baseimagear[i][j + 1], baseimagear[i][j + 2]},
-				{baseimagear[i + 1][j], baseimagear[i + 1][j + 1], baseimagear[i + 1][j + 2]},
-				{baseimagear[i + 2][j], baseimagear[i + 2][j + 1], baseimagear[i + 2][j + 2]}
-			};
+			vector<vector<double>> tempmatrix[wallyrows][wallycols];
 
-			double tempans = CalcSSD(wallyar, tempmatrix, 3);
-				cout << tempans << endl;
+			for (int a = 0; a <= wallyrows - 1; a++) {
+				for (int b = 0; b <= wallycols - 1; b++) {
 
+					tempmatrix[a][b] = baseimagear[i + a][j + b];
+
+				}
+			}
+			double tempans = CalcSSD(wallyar, tempmatrix);
+			cout << tempans << endl;
+			memset(tempmatrix, 0, sizeof(tempmatrix[0][0]) * wallyrows * wallycols);
 		};
 	};
-
-
 }
