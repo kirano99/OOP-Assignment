eadeeb45af86ae1213989b4fa7f4ee7cf5866663
kirano99 <34285686+kirano99@users.noreply.github.com>
2018-12-18 10:57:36 +0000

    Fixed Code

--- OOP Assignment 15.8/Assessment VS15.8/Assessment VS15.8/MainHeader.h
+++ OOP Assignment 15.8/Assessment VS15.8/Assessment VS15.8/MainHeader.h
@@ -1,5 +1,103 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <windows.h>
+using namespace std;
+
+//loading animation function
+void loading()
+{
+	std::cout << '-' << std::flush;
+	for (;;) {
+		Sleep(10);
+		std::cout << "\b\\" << std::flush;
+		Sleep(10);
+		std::cout << "\b|" << std::flush;
+		Sleep(10);
+		std::cout << "\b/" << std::flush;
+		Sleep(10);
+		std::cout << "\b-" << std::flush;
+	}
+}
 
 //cpp functions
 double* read_text(char *fileName, int sizeR, int sizeC);
 void write_pgm(char *filename, double *data, int sizeR, int sizeC, int Q);
+
+//classes
+class NNS {
+public:
+	double CalcSSD(vector<unsigned char> matrix1, vector<unsigned char> matrix2, int wallycolums, int wallyrows) {
+		double SumSquareDiff = 0;
+		double difference = 0;
+		for (int i = 0; i <= wallycolums - 1; i++) {
+			for (int j = 0; j <= wallycolums - 1; j++) {
+				int k = i * wallycolums + j;
+				if (matrix1[k] == 255) {
+					matrix1[k] = matrix2[k];
+				}
+				difference = matrix1[k] - matrix2[k];
+				SumSquareDiff += difference * difference;
+			}
+		}
+		return SumSquareDiff;
+	}
+	vector <unsigned char> tempmatrix;
+};
+
+class Base_Image {
+public:
+	double highlightedscene[786432];
+};
+
+class Ref_Image : public Base_Image {
+private:
+	int rows;
+	int cols;
+
+public:
+
+	Ref_Image() {
+		rows = 49;
+		cols = 36;
+		cout << "Ref_Image obj created" << endl;
+	}
+	virtual ~Ref_Image() {
+		cout << "Ref_Image obj deleted" << endl;
+	}
+	const int getrows() {
+		return rows;
+	}
+	const int getcols() {
+		return cols;
+	}
+};
+
+class Large_Image : public Base_Image {
+
+public:
+	int rows;
+	int cols; 
+
+	Large_Image() {
+		rows = 768;
+		cols = 1024;
+		cout << "Large_Image obj created" << endl;
+	}
+	virtual ~Large_Image() {
+		cout << "Large_Image obj deleted" << endl;
+	}
+	const int getrows() {
+		return rows;
+	}
+	const int getcols() {
+		return cols;
+	}
+};
+
+class Matrix {
+public:
+	double * basereturndata;
+	double * wallyreturndata;
+};
--- OOP Assignment/OOP Assignment/ReadWrite.cpp
+++ OOP Assignment/OOP Assignment/ReadWrite.cpp
@@ -37,40 +37,40 @@ double* read_text(char *fileName, int sizeR, int sizeC)
 	// Converts a 1D array of doubles of size R*C to .pgm image of R rows and C Columns 
 	// and stores .pgm in filename
 	// Use Q = 255 for greyscale images and 1 for binary images.
-	void write_pgm(char *filename, double *data, int sizeR, int sizeC, int Q)
-	{
+void write_pgm(char *filename, double *data, int sizeR, int sizeC, int Q)
+{
 
-		int i, j;
-		unsigned char *image;
-		ofstream myfile;
+	int i, j;
+	unsigned char *image;
+	ofstream myfile;
 
-		image = (unsigned char *) new unsigned char[sizeR*sizeC];
+	image = (unsigned char *) new unsigned char[sizeR*sizeC];
 
-		// convert the integer values to unsigned char
+	// convert the integer values to unsigned char
 
-		for (i = 0; i < sizeR*sizeC; i++)
-			image[i] = (unsigned char)data[i];
+	for (i = 0; i < sizeR*sizeC; i++)
+		image[i] = (unsigned char)data[i];
 
-		myfile.open(filename, ios::out | ios::binary | ios::trunc);
+	myfile.open(filename, ios::out | ios::binary | ios::trunc);
 
-		if (!myfile) {
-			cout << "Can't open file: " << filename << endl;
-			exit(1);
-		}
-
-		myfile << "P5" << endl;
-		myfile << sizeC << " " << sizeR << endl;
-		myfile << Q << endl;
-
-		myfile.write(reinterpret_cast<char *>(image), (sizeR*sizeC) * sizeof(unsigned char));
-
-		if (myfile.fail()) {
-			cout << "Can't write image " << filename << endl;
-			exit(0);
-		}
-
-		myfile.close();
-
-		delete[] image;
-
+	if (!myfile) {
+		cout << "Can't open file: " << filename << endl;
+		exit(1);
 	}
+
+	myfile << "P5" << endl;
+	myfile << sizeC << " " << sizeR << endl;
+	myfile << Q << endl;
+
+	myfile.write(reinterpret_cast<char *>(image), (sizeR*sizeC) * sizeof(unsigned char));
+
+	if (myfile.fail()) {
+		cout << "Can't write image " << filename << endl;
+		exit(0);
+	}
+
+	myfile.close();
+
+	delete[] image;
+
+}
